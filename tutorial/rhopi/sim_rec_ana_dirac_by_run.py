76a97bccce817898d895eb518c5c0fb864c2bc9b
Zhao Xianghu <zhaoxh@ihep.ac.cn>
2015-01-06 10:51:47 +0800

    modify the tutorial a little

--- tutorial/rhopi/sim_rec_ana_dirac_by_run.py
+++ tutorial/rhopi/sim_rec_ana_dirac_by_run.py
@@ -7,13 +7,11 @@ jobGroup = 'simrecana_rhopi_140512'
 metadata = {'resonance': 'jpsi', 'eventType': 'rhopi'}
 splitter = UserSplitterByRun(evtMaxPerJob = 100, evtTotal = 100*10)
 
-app = Boss(version=bossVersion, optsfile=optionsFile, recoptsfile=recOptionsFile, anaoptsfile=anaOptionsFile, metadata=metadata)
+app = Boss(version=bossVersion, optsfile=optionsFile, recoptsfile=recOptionsFile, anaoptsfile=anaOptionsFile, metadata=metadata, use_custom_package=True)
 j = Job(application=app, splitter=splitter)
 
 j.backend = Dirac()
 j.backend.settings['JobGroup'] = jobGroup
-# add lib files needed for the analysis
-j.inputsandbox.append('/ihepbatch/bes/zhaoxh/boss/664p02/workarea/InstallArea/x86_64-slc5-gcc43-opt/lib/libRhopiAlg.so')
 j.submit()
 
 print '\nThe DFC path for output data is:'
